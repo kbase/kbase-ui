203ed47979e0fc737e9bba3a2257822a6466dbc5
Harry Yoo <hsyoo@anl.gov>
2016-01-15 10:55:35 -0600

    bug fix: incorrect namespace caused original search mal-function

--- src/search/js/homology.js
+++ src/search/js/homology.js
@@ -48,7 +48,7 @@ homologyApp.config(function($httpProvider,$stateProvider,$provide) {
             return $delegate;
         }]);
 
-        
+
         $(document).ajaxStop($.unblockUI());
     }
 );
@@ -59,7 +59,7 @@ homologyApp.factory("require", function($rootScope) {
         successCallback = (successCallback || angular.noop);
         errorCallback = (errorCallback || angular.noop);
 
-        require( (dependencies || []), 
+        require( (dependencies || []),
             function successCallbackProxy() {
                 var args = arguments;
 
@@ -106,7 +106,7 @@ homologyApp.service('searchKBaseClientsService', function($q, $http, $rootScope)
  *  This service houses the various options captured for Search, some of which
  *  are housed in local storage for persistence.
  */
-homologyApp.service('searchOptionsService', function searchOptionsService() {
+homologyApp.service('homologyOptionsService', function homologyOptionsService() {
     var genomesWorkspace = "KBasePublicGenomesV4";
     var searchGenomesWorkspace = "KBasePublicRichGenomesV4";
     var metagenomesWorkspace = "wilke:Data";
@@ -279,8 +279,8 @@ homologyApp.filter('displayPairwiseComparison', function($sce) {
 /*
  *  The main Search controller that is responsible for content inside the Search view.
  */
-homologyApp.controller('homologyController', function searchCtrl($rootScope, $scope, $q, $timeout, $http, $state, $stateParams, searchCategoryLoadService, searchOptionsService, searchKBaseClientsService, require) {
-    $scope.options = searchOptionsService;
+homologyApp.controller('homologyController', function searchCtrl($rootScope, $scope, $q, $timeout, $http, $state, $stateParams, homologyOptionsService, searchKBaseClientsService, require) {
+    $scope.options = homologyOptionsService;
     $scope.workspace_service;
     $scope.narrative_manager;
 
@@ -1806,4 +1806,3 @@ homologyApp.controller('homologyController', function searchCtrl($rootScope, $sc
     };
 
 });
-
