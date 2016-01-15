1743358adbe58f0453e2e51d366866264b7beb8e
Erik Pearson <eapearson@lbl.gov>
2016-01-15 11:54:46 -0800

    improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
-mkdir -p /kb/deployment/services/kbase-ui
-cp -pr dist/client/* /kb/deployment/services/kbase-ui
+DEPLOY_DEST=/kb/deployment/services/kbase-ui
+rm -rf $DEPLOY_DEST
+mkdir -p $DEPLOY_DEST
+cp -pr build/dist/client/* $DEPLOY_DEST
