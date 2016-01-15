ddd615cb2a9fb5b37bc8a4421ddaf7393a5ec211
Erik Pearson <eapearson@lbl.gov>
2016-01-15 15:26:55 -0800

    fix coverage config

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -43,7 +43,7 @@ module.exports = function (config) {
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
-          'build/client/modules/!(bower_components)/**/*.js': ['coverage']
+          'build/build/client/modules/!(bower_components)/**/*.js': ['coverage']
         },
 
         coverageReporter: {
