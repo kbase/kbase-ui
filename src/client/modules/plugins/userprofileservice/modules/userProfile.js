726301acd4235826fcd606f42e0e2084c92c24eb
Bill Riehl <wjriehl@lbl.gov>
2016-01-20 18:26:27 -0800

    Fixed case problem with UserProfile client (should be userProfile)

--- src/client/modules/plugins/userprofileservice/modules/userProfile.js
+++ src/client/modules/plugins/userprofileservice/modules/userProfile.js
@@ -5,7 +5,7 @@ define([
     'bluebird',
     'kb/common/utils',
     'md5',
-    'kb/service/client/UserProfile',
+    'kb/service/client/userProfile',
     'kb_plugin_userprofileservice'
 ],
     function (Promise, Utils, md5, UserProfileService, Plugin) {
