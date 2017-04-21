6a7cb14be69d2776f45f79afd7099398ecede329
Erik Pearson <eapearson@lbl.gov>
2017-04-21 15:35:08 -0700

    Add dirty-flag extension for knockout

--- src/client/modules/lib/knockout-plus.js
+++ src/client/modules/lib/knockout-plus.js
@@ -3,6 +3,27 @@ define([
     'knockout-mapping',
     'knockout-arraytransforms',
     'knockout-validation'
-], function(ko) {
+], function (ko) {
+
+
+    ko.extenders.dirty = function (target, startDirty) {
+        var cleanValue = ko.observable(ko.mapping.toJSON(target));
+        var dirtyOverride = ko.observable(ko.utils.unwrapObservable(startDirty));
+
+        target.isDirty = ko.computed(function () {
+            return dirtyOverride() || ko.mapping.toJSON(target) !== cleanValue();
+        });
+
+        target.markClean = function () {
+            cleanValue(ko.mapping.toJSON(target));
+            dirtyOverride(false);
+        };
+        target.markDirty = function () {
+            dirtyOverride(true);
+        };
+
+        return target;
+    };
+
     return ko;
-})
+});
