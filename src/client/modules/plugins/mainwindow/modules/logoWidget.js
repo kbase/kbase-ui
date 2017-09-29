3992c3c16e86848ab8831177f17969273a8e77b0
Erik Pearson <eapearson@lbl.gov>
2017-09-29 11:06:50 -0700

    Simplfy header layout, use flexbox layout, add product label "hub" [TASK-1105]

--- src/client/modules/plugins/mainwindow/modules/logoWidget.js
+++ src/client/modules/plugins/mainwindow/modules/logoWidget.js
@@ -1,35 +1,65 @@
-/*global define */
-/*jslint white: true, browser: true */
 define([
-    'kb_widget/bases/simpleWidget',
     'kb_common/html',
     'kb_plugin_mainWindow'
-],
-    function (SimpleWidget, html, Plugin) {
-        'use strict';
+], function (
+    html,
+    Plugin
+) {
+    'use strict';
+    var t = html.tag,
+        a = t('a'),
+        img = t('img'),
+        div = t('div'),
+        span = t('span');
 
-        function myWidget(config) {
-            return SimpleWidget.make({
-                runtime: config.runtime,
-                on: {
-                    start: function () {
-                        this.setState('test', true);
-                    },
-                    render: function () {
-                        var a = html.tag('a'),
-                            img = html.tag('img'),
-                            div = html.tag('div');
-                        return div({class: 'kb-logo-widget'}, a({href: this.runtime.config('resources.docSite.base.url')}, [
-                            img({id: 'logo', src: Plugin.plugin.fullPath + '/images/kbase_logo.png', width: '46'})
-                        ]));
-                    }
-                }
-            });
+    function factory(config) {
+        var hostNode, container, runtime = config.runtime;
+
+        function attach(node) {
+            hostNode = node;
+            container = hostNode.appendChild(document.createElement('div'));
+            container.classList.add('widget-logo');
+        }
+
+        function start() {
+            container.innerHTML = [
+                a({
+                    href: runtime.config('resources.docSite.base.url'),
+                    class: '-logo'
+                }, img({
+                    src: Plugin.plugin.fullPath + '/images/kbase_logo.png',
+                    width: '46px'
+                })),
+                div({
+                    class: '-label'
+                }, [
+                    'hub',
+                    span({
+                        class: 'fa fa-chevron-right',
+                        style: {
+                            color: 'silver'
+                        }
+                    })
+                ])
+            ].join('');
+        }
+
+        function detach() {
+            if (hostNode && container) {
+                hostNode.removeChild(container);
+            }
         }
 
         return {
-            make: function (config) {
-                return myWidget(config);
-            }
+            attach: attach,
+            start: start,
+            detach: detach,
         };
-    });
+    }
+
+    return {
+        make: function (config) {
+            return factory(config);
+        }
+    };
+});
