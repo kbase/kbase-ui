30c3901722cfbbe6d4182db0942ed9b3f37d7d91
Erik Pearson <eapearson@lbl.gov>
2017-11-30 08:58:40 -0800

    udpated search (jgi and data) [TASK-1210, TASK-921] - data search fixed history dropdown feature - jgi search improved staging, staging monitoring, and incorpoprated serverl features and styles from data search

--- src/client/modules/lib/knockout-plus.js
+++ src/client/modules/lib/knockout-plus.js
@@ -261,6 +261,88 @@ define([
         }
     };
 
+    function niceDuration(value, options) {
+        options = options || {};
+        var minimized = [];
+        var units = [{
+            unit: 'millisecond',
+            short: 'ms',
+            single: 'm',
+            size: 1000
+        }, {
+            unit: 'second',
+            short: 'sec',
+            single: 's',
+            size: 60
+        }, {
+            unit: 'minute',
+            short: 'min',
+            single: 'm',
+            size: 60
+        }, {
+            unit: 'hour',
+            short: 'hr',
+            single: 'h',
+            size: 24
+        }, {
+            unit: 'day',
+            short: 'day',
+            single: 'd',
+            size: 30
+        }];
+        var temp = Math.abs(value);
+        var parts = units
+            .map(function (unit) {
+                // Get the remainder of the current value
+                // sans unit size of it composing the next
+                // measure.
+                var unitValue = temp % unit.size;
+                // Recompute the measure in terms of the next unit size.
+                temp = (temp - unitValue) / unit.size;
+                return {
+                    name: unit.single,
+                    unit: unit.unit,
+                    value: unitValue
+                };
+            }).reverse();
+
+        parts.pop();
+
+        // We skip over large units which have not value until we
+        // hit the first unit with value. This effectively trims off
+        // zeros from the end.
+        // We also can limit the resolution with options.resolution
+        var keep = false;
+        for (var i = 0; i < parts.length; i += 1) {
+            if (!keep) {
+                if (parts[i].value > 0) {
+                    keep = true;
+                    minimized.push(parts[i]);
+                }
+            } else {
+                minimized.push(parts[i]);
+                if (options.resolution &&
+                    options.resolution === parts[i].unit) {
+                    break;
+                }
+            }
+        }
+
+        if (minimized.length === 0) {
+            // This means that there is are no time measurements > 1 second.
+            return '<1s';
+        } else {
+            // Skip seconds if we are into the hours...
+            // if (minimized.length > 2) {
+            //     minimized.pop();
+            // }
+            return minimized.map(function (item) {
+                return String(item.value) + item.name;
+            })
+                .join(' ');
+        }
+    }
+
     ko.bindingHandlers.typedText = {
         update: function (element, valueAccessor) {
             var value = valueAccessor();
@@ -286,8 +368,13 @@ define([
                     formatted = missing;
                 } else {
                     switch (format) {
+                    case 'elapsed':
                     case 'nice-elapsed':
                         formatted = Utils.niceElapsedTime(moment(valueUnwrapped).toDate());
+                        break;
+                    case 'duration':
+                        // formatted = Utils.niceElapsedTime(moment(valueUnwrapped).toDate());
+                        formatted = niceDuration(valueUnwrapped);
                         break;
                     default: formatted = moment(valueUnwrapped).format(format);
                     }
