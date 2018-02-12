71d05ddfeb03b62f471899516c250ffb0d7b060b
Erik Pearson <eapearson@lbl.gov>
2018-02-12 10:14:32 -0800

    remove old search support and redirect to new [SCT-554] - the entire search directory removed from source - external library dependencies only required by search have been removed - build tool adjusted accordingly - simple index page which redirects to new the new search

--- src/search/src/postal.request-response.q.js
+++
@@ -1,8 +0,0 @@
-define(['postal.request-response', 'q'], function (postal, Q) {
-  postal.configuration.promise.createDeferred = function () {
-    return Q.defer();
-  };
-  postal.configuration.promise.getPromise = function (deferred) {
-    return deferred.promise;
-  };
-});
