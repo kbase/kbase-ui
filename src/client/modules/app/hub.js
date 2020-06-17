c453f8bdc9f6afa1b3df91e58248c738e28087c9
Erik Pearson <eapearson@lbl.gov>
2020-06-17 10:58:57 -0700

    remove a few unused styles (really just to trigger a build since at this point some plugins are referened by master branch.)

--- src/client/modules/app/hub.js
+++ src/client/modules/app/hub.js
@@ -1,7 +1,8 @@
 /*
 hub - the kbase ui web app
-This module wraps the simple kbase ui app, adding plugins and services from the config,
-some default behavior, and launching the initial widget.
+
+This module wraps the simple kbase ui app and controls it. This control is currently
+quite limited.
 
 It incorporates much of what used to be encoded into app.js, but in order to have
 app be itself testable, much needed to be stripped out and placed into an app-specific
