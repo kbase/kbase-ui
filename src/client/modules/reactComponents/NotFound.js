d2720df2dfb34f7febaff51c61be313091cf0947
Erik Pearson <eapearson@lbl.gov>
2020-06-25 18:32:35 -0700

    Add a new, simply, path not found component.

--- src/client/modules/reactComponents/NotFound.js
+++ src/client/modules/reactComponents/NotFound.js
@@ -14,10 +14,15 @@ define([
 
     class NotFound extends Component {
         render() {
+            this.props.runtime.send('ui', 'setTitle', 'Not Found!');
             return html`
-                <div className="alert alert-danger">
-                <p>Path not found</p>
-                <p>${this.props.path}</p>
+                <div className="well" style=${{margin: '0 10px'}}>
+                    <div className="text-danger"  style=${{fontSize: '140%'}}>
+                        <strong><span className="fa fa-meh-o"></span>${' '}Path Not Found</strong>
+                    </div>
+                    <p className="text-danger" style=${{fontSize: '140%', marginTop: '10px'}}>
+                        Sorry, the path "${this.props.params.request.original}" does not lead anywhere.
+                    </p>
                 </div>
             `;
         }
