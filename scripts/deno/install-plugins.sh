3b791eab33623969dde0b65469d0ac15eb864987
Erik Pearson <eapearson@lbl.gov>
2021-11-16 08:59:06 -0800

    add debugging echos

--- scripts/deno/install-plugins.sh
+++ scripts/deno/install-plugins.sh
@@ -1,4 +1,13 @@
 cd tools/deno-cli
-DIR=`pwd`/../.. docker-compose run \
+export DIR=`pwd`/../.. 
+export ABS_DIR=$(cd ${DIR}; pwd)
+echo
+echo "Installing plugins"
+echo "DIR=$DIR"
+echo "ABS_DIR=$ABS_DIR"
+echo
+ls $DIR
+echo
+docker-compose run \
     deno run --unstable --allow-run --allow-write --allow-read \
     /app/scripts/deno/install-plugins.ts /app/config/plugins.yaml /app/build/dist/modules
