bf418ce1a0873907c2faff990918069b3329e478
Erik Pearson <eapearson@lbl.gov>
2021-11-16 09:02:06 -0800

    try abs dir

--- scripts/deno/install-plugins.sh
+++ scripts/deno/install-plugins.sh
@@ -1,10 +1,10 @@
 cd tools/deno-cli
-export DIR=`pwd`/../.. 
-export ABS_DIR=$(cd ${DIR}; pwd)
+export REL_DIR=`pwd`/../.. 
+export DIR=$(cd ${DIR}; pwd)
 echo
 echo "Installing plugins"
+echo "REL_DIR=$REL_DIR"
 echo "DIR=$DIR"
-echo "ABS_DIR=$ABS_DIR"
 echo
 ls $DIR
 echo
