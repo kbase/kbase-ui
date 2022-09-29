754de9f75e3fb398b230047b4cfaef93b8883301
David Lyon <dauglyon@users.noreply.github.com>
2022-09-29 11:39:14 -0700

    add (hacky) listener for europa navigate events

--- src/client/modules/app/services/europa.ts
+++ src/client/modules/app/services/europa.ts
@@ -1,11 +1,14 @@
 import { Runtime } from '../../lib/types';
 
+// TODO: DO NOT MERGE WITHOUT SAFER TARGET ORIGIN
+const europaTargetOrigin = '*'; // 'https://ci-europa.kbase.us'
+
 export const initEuropaRoutes = (runtime: Runtime) => {
     runtime.receive('app', 'route-component', (payload) => {
         if (window.parent) {
             window.parent.postMessage(
                 { source: 'kbase-ui.app.route-component', payload },
-                'https://ci-europa.kbase.us'
+                europaTargetOrigin
             );
         }
     });
@@ -13,8 +16,17 @@ export const initEuropaRoutes = (runtime: Runtime) => {
         if (window.parent) {
             window.parent.postMessage(
                 { source: 'kbase-ui.ui.setTitle', payload },
-                'https://ci-europa.kbase.us'
+                europaTargetOrigin
             );
+        }
+    });
+    window.addEventListener('message', (message) => {
+        if (message.source !== window.parent) return;
+        if (
+            message?.data?.source &&
+            message?.data?.source == 'europa.navigate'
+        ) {
+            runtime.send('app', 'navigate', message.data.payload);
         }
     });
 };
