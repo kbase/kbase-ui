c027ec5cbc753c6ce79e96e18ec3c069859eb255
David Lyon <dauglyon@users.noreply.github.com>
2022-10-06 10:55:35 -0700

    set target to _top on AutoPostForm, rename europa init func

--- src/client/modules/app/services/europa.ts
+++ src/client/modules/app/services/europa.ts
@@ -3,7 +3,7 @@ import { Runtime } from '../../lib/types';
 // TODO: DO NOT MERGE WITHOUT SAFER TARGET ORIGIN
 const europaTargetOrigin = '*'; // 'https://ci-europa.kbase.us'
 
-export const initEuropaRoutes = (runtime: Runtime) => {
+export const initEuropa = (runtime: Runtime) => {
     runtime.receive('app', 'route-component', (payload) => {
         if (window.parent) {
             window.parent.postMessage(
