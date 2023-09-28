4bb9a48265b83d4c444f874011f987af09a4f8b3
David Lyon <dauglyon@users.noreply.github.com>
2023-09-28 12:35:07 -0700

    More secure message targets, implement europa.identify receiver

--- src/client/modules/app/services/europa.ts
+++ src/client/modules/app/services/europa.ts
@@ -1,14 +1,29 @@
 import { Runtime } from '../../lib/types';
 
-// TODO: DO NOT MERGE WITHOUT SAFER TARGET ORIGIN
-const europaTargetOrigin = '*'; // 'https://ci-europa.kbase.us'
+// This line assumes kbase-ui is running on a direct subdomain on the Europa instance
+// this subdomain relationship is _required_ for CORS security policy reasons
+// i.e. legacy.ci-europa.kbase.us --> ci-europa.kbase.us
+const europaTargetOrigin = window.location.hostname
+    .split('.')
+    .slice(1)
+    .join('.');
+
+// This variable is undefined by default but will be set if
+// the iframe receives a 'europa.identify' message from a parent
+// which specifies a parent domain which is not the one above.
+// WHEN TRUTHY, token events will not propagate to the parent
+// This enables development in localhost.
+let insecureParent: string | undefined = undefined;
+
+const getMessageDomain = () =>
+    insecureParent ? insecureParent : europaTargetOrigin;
 
 export const initEuropa = (runtime: Runtime) => {
     runtime.receive('app', 'route-component', (payload) => {
         if (window.parent) {
             window.parent.postMessage(
                 { source: 'kbase-ui.app.route-component', payload },
-                europaTargetOrigin
+                getMessageDomain()
             );
         }
     });
@@ -16,12 +31,12 @@ export const initEuropa = (runtime: Runtime) => {
         if (window.parent) {
             window.parent.postMessage(
                 { source: 'kbase-ui.ui.setTitle', payload },
-                europaTargetOrigin
+                getMessageDomain()
             );
         }
     });
     runtime.receive('session', 'loggedin', () => {
-        if (window.parent) {
+        if (window.parent && !insecureParent) {
             window.parent.postMessage(
                 {
                     source: 'kbase-ui.session.loggedin',
@@ -34,12 +49,19 @@ export const initEuropa = (runtime: Runtime) => {
         }
     });
     window.addEventListener('message', (message) => {
-        if (message.source !== window.parent) return;
-        if (
-            message?.data?.source &&
-            message?.data?.source == 'europa.navigate'
-        ) {
+        // only look at messages which come from the iframe parent
+        if (message.source !== window.parent || !message?.data?.source) return;
+        // Navigate events
+        if (message?.data?.source == 'europa.navigate') {
             runtime.send('app', 'navigate', message.data.payload);
+        }
+        // Domain identify events
+        else if (message?.data?.source == 'europa.identify') {
+            if (message.data.payload !== europaTargetOrigin) {
+                // we could allow only specific domains here
+                // (i.e. localhost:3000) in the future
+                insecureParent = message.data.payload;
+            }
         }
     });
 };
