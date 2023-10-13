738eec9c69e469695c37f1319811d19331c37e32
Erik Pearson <eapearson@lbl.gov>
2023-10-13 18:45:02 +0000

    remove unused tools [CE-291]

--- tools/dist-proxy/contents/maybe-install-self-signed-cert.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-if [ ! -f /kb/deployment/ssl/test.crt ]; then
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /kb/deployment/ssl/test.key -out /kb/deployment/ssl/test.crt -subj "/C=US/ST=California/L=Berkeley/O=LBNL/OU=KBase/CN=ci.kbase.us"
-fi
